b83c531ec27fedae34261fcf3c4bd4ffd0e08962
iyonissio <iyonissio01@gmail.com>
2024-09-13 02:52:28 +0800

    add  all code

--- lib/ui/onboarding_screen.dart
+++ lib/ui/onboarding_screen.dart
@@ -12,7 +12,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Onboarding Screen'),
+        title: const Text('Onboarding Screeen'),
       ),
     );
   }
